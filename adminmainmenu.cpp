3de778046fdd742f83b3e0900efb0abbfaee912e
AjaxGroup1400 <uplayggboy@gmail.com>
2022-07-10 18:36:08 +0430

    added new ui and some other changes

--- adminmainmenu.cpp
+++ adminmainmenu.cpp
@@ -17,24 +17,41 @@ AdminMainMenu::AdminMainMenu( QString firstName , AdminMainMenu * member , QWidg
 {
     ui->setupUi(this);
     this->ui->pushButton->setStyleSheet("background-color: transparent");
+
     this->ui->pushButton_3->setStyleSheet("background-color: transparent");
+
     this->ui->pushButton_4->setStyleSheet("background-color: transparent");
+
     this->ui->pushButton_7->setStyleSheet("background-color: transparent");
+
     this->ui->label_14->setStyleSheet("background-color: #f0f0f0; border-radius: 20px");
+
     this->ui->pushButton_5->setStyleSheet("background-color: transparent");
+
     this->ui->pushButton_6->setStyleSheet("background-color: transparent");
 
+    this->ui->pushButton_8->setStyleSheet("background-color: transparent");
+
+
     this->mainmenu = member;
+
     if(member!=nullptr)
     {
         this->set_username(mainmenu->get_username());
+
         this->set_password(mainmenu->get_password());
+
         this->set_last_name(mainmenu->get_last_name());
+
         this->set_role(mainmenu->get_role());
+
         this->set_national_code(mainmenu->get_national_code());
+
         this->set_phone_number(mainmenu->get_phone_number());
     }
+
     this->set_first_name(firstName);
+
     this->ui->label_2->setText("Hi dear " + this->get_first_name());
 }
 
@@ -48,20 +65,29 @@ AdminMainMenu::~AdminMainMenu()
 void AdminMainMenu::on_pushButton_5_clicked()
 {
     QMessageBox* exit = new QMessageBox(QMessageBox::Warning,"Close Window","Do you want to close this window?");
+
     exit->setStandardButtons(QMessageBox::Yes);
+
     exit->addButton(QMessageBox::No);
+
     exit->setDefaultButton(QMessageBox::No);
+
     exit->show();
+
     if(exit->exec() == QMessageBox::Yes){
         LoginPage* lg = new LoginPage;
+
         lg->show();
+
         exit->close();
+
         connect(exit ,&QMessageBox::buttonClicked ,exit ,&QMessageBox::deleteLater);
 
         close();
     }
     else{
         exit->close();
+
         connect(exit ,&QMessageBox::buttonClicked ,exit ,&QMessageBox::deleteLater);
 
     }
@@ -72,7 +98,9 @@ void AdminMainMenu::on_pushButton_5_clicked()
 void AdminMainMenu::on_pushButton_clicked()
 {    
     AdminProfile* ap = new AdminProfile(this);
+
     ap->show();
+
     close();
 }
 
@@ -80,7 +108,9 @@ void AdminMainMenu::on_pushButton_clicked()
 void AdminMainMenu::on_pushButton_4_clicked()
 {
     AdminSendAssertion* asa = new AdminSendAssertion(this);
+
     asa->show();
+
     close();
 }
 
@@ -88,7 +118,9 @@ void AdminMainMenu::on_pushButton_4_clicked()
 void AdminMainMenu::on_pushButton_3_clicked()
 {
     AdminAddPeople* aap = new AdminAddPeople(this);
+
     aap->show();
+
     close();
 }
 
@@ -106,7 +138,9 @@ void AdminMainMenu::on_pushButton_2_clicked()
 void AdminMainMenu::on_pushButton_7_clicked()
 {
     adminAddClass* adc = new adminAddClass(this);
+
     adc->show();
+
     close();
 }
 
@@ -114,7 +148,25 @@ void AdminMainMenu::on_pushButton_7_clicked()
 void AdminMainMenu::on_pushButton_6_clicked()
 {
     AdminClassInfo* aci = new AdminClassInfo(this);
+
     aci->show();
+
     close();
+}
+
+
+void AdminMainMenu::on_pushButton_8_clicked()
+{
+    QMessageBox* exit = new QMessageBox(QMessageBox::Warning,"Finish term","Do you want to finish the term?");
+    exit->setStandardButtons(QMessageBox::Yes);
+    exit->addButton(QMessageBox::No);
+    exit->setDefaultButton(QMessageBox::No);
+    exit->show();
+    if(exit->exec() == QMessageBox::Yes){
+
+    }
+    else{
+        exit->close();
+    }
 }
 
